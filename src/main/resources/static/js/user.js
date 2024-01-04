03560a4300a0c0d569b1b196bb5108f1fefb5dc4
Siterizer <siterizer@gmail.com>
2024-01-04 20:36:21 +0100

    create licence buy POST request frontend part (right now without any paying availability)

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,15 +1,6 @@
 $(document).ready(function() {
     fill_products_table()
-
-    $.get("user/info", function( data ) {
-        $("#user_email").html(data.email);
-
-        for(let i = 0; i<data.licences.length; i++){
-            $(user_products).find('tbody').append("<tr><td>" + data.licences[i].name +"</td>" +
-            "<td>" + data.licences[i].licence + "</td>" +
-            "</tr>");
-        }
-    });
+    show_user_info()
 });
 $("#changePassword").submit(function(event) {
     var ajaxRequest;
@@ -30,13 +21,48 @@ $("#changePassword").submit(function(event) {
         $("#result").html("error     " + data['responseText']);
     });
 });
+function show_user_info(){
+    $.get("user/info", function( data ) {
+        $("#user_email").html(data.email);
+
+        for(let i = 0; i<data.licences.length; i++){
+            $(user_licences).find('tbody').append("<tr><td>" + data.licences[i].name +"</td>" +
+            "<td>" + data.licences[i].licence + "</td>" +
+            "</tr>");
+        }
+    });
+}
 function fill_products_table(){
     $.get("products", function( data ) {
         for(let i = 0; i<data.length; i++){
             $(products_array).find('tbody').append("<tr><td>" + data[i].id +"</td>" +
             "<td>" + data[i].name + "</td>" +
-            "<td><button type='button' value='submit'>Buy</button></td>" +
+            "<td><button type='button' onclick='buy_licence(\"" + data[i].id + "\")' id='" + data[i].id + "' value='submit'>Submit</button></td>" +
+
             "</tr>");
         }
     });
+}
+function buy_licence(id) {
+    var ajaxRequest;
+    event.preventDefault();
+    $("#result").html('');
+    var values = $(this).serialize();
+       ajaxRequest= $.ajax({
+            url: "licence/buy/",
+            type: "post",
+            contentType : "application/json",
+            data: JSON.stringify(id)
+       });
+    ajaxRequest.done(function (response){
+         $("#result").html('Submitted successfully');
+         clear_licence_table();
+         show_user_info();
+    });
+    ajaxRequest.fail(function (data){
+        $("#result").html("error     " + data['responseText']);
+    });
+}
+function clear_licence_table(){
+    $(user_licences).find('tbody').empty();
 }
